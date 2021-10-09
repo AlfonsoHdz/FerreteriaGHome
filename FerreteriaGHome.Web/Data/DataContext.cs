d96274e8d223687a5f6a6ac02de3d08ebbcfd531
Alfonso Hdz Espinosa <alfonsinic2013@gmail.com>
2021-10-09 12:22:46 -0500

    Creacion del DataContext

--- FerreteriaGHome.Web/Data/DataContext.cs
+++ FerreteriaGHome.Web/Data/DataContext.cs
@@ -5,7 +5,27 @@ using System.Threading.Tasks;
 
 namespace FerreteriaGHome.Web.Data
 {
+    using FerreteriaGHome.Web.Data.Entities;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
     public class DataContext
     {
+        public DbSet<Clients> Clients { get; set; }
+
+        public DbSet<Products> Products { get; set; }
+
+        public DbSet<Provider> Providers { get; set; }
+        public DbSet<SaleDetail> SaleDetails { get; set; }
+        public DbSet<Sales> Sales { get; set; }
+        public DbSet<SalesAgent> SalesAgents { get; set; }
+
+        public DbSet<Shopping> Shoppings { get; set; }
+        public DbSet<ShoppingDetail> ShoppingDetails { get; set; }
+      
+
+
+
+
     }
 }
