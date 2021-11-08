d16a74d194e79d4f7e4b975e462c763979cfd1a0
Alfonso Hdz Espinosa <alfonsinic2013@gmail.com>
2021-11-08 11:05:19 -0600

    s

--- FerreteriaGHome.Web/Startup.cs
+++ FerreteriaGHome.Web/Startup.cs
@@ -79,6 +79,7 @@ namespace FerreteriaGHome.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseAuthentication();
