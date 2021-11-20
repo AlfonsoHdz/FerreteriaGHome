cdf99e25177864eb2a6802f91a4e27b39ea42281
Alfonso Hdz Espinosa <alfonsinic2013@gmail.com>
2021-11-20 00:01:02 -0600

    Edicion de OrderController

--- FerreteriaGHome.Web/Controllers/OrdersController.cs
+++ FerreteriaGHome.Web/Controllers/OrdersController.cs
@@ -1,4 +1,5 @@
 ﻿using FerreteriaGHome.Web.Data;
+using FerreteriaGHome.Web.Data.Entities;
 using FerreteriaGHome.Web.Helper;
 using FerreteriaGHome.Web.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -63,7 +64,7 @@ namespace FerreteriaGHome.Web.Controllers
             return View(model);
         }
 
-
+        [HttpPost]
         public async Task<IActionResult> addProduct(AddItemViewModel model)
         {
             if (this.ModelState.IsValid)
@@ -112,6 +113,113 @@ namespace FerreteriaGHome.Web.Controllers
 
             }
             return this.View(model);
+
+
+        }
+
+        public async Task<IActionResult> DeleteItem(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orderDetailTemp = await this.datacontext.OrderDetailTemps.FindAsync(id);
+
+            if (orderDetailTemp == null)
+            {
+                return NotFound();
+            }
+
+            this.datacontext.OrderDetailTemps.Remove(orderDetailTemp);
+            await this.datacontext.SaveChangesAsync();
+            return this.RedirectToAction("Create");
+        }
+
+        public async Task<IActionResult> Increase(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orderDetailTemp = await this.datacontext.OrderDetailTemps.FindAsync(id);
+
+            if (orderDetailTemp == null)
+            {
+                return NotFound();
+            }
+            orderDetailTemp.Quantity += 1;
+            this.datacontext.OrderDetailTemps.Update(orderDetailTemp);
+            await this.datacontext.SaveChangesAsync();
+            return this.RedirectToAction("Create");
+        }
+
+        public async Task<IActionResult> Decrease(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orderDetailTemp = await this.datacontext.OrderDetailTemps.FindAsync(id);
+
+            if (orderDetailTemp == null)
+            {
+                return NotFound();
+            }
+            orderDetailTemp.Quantity -= 1;
+            if (orderDetailTemp.Quantity > 0)
+            {
+                this.datacontext.OrderDetailTemps.Update(orderDetailTemp);
+                await this.datacontext.SaveChangesAsync();
+            }
+
+            return this.RedirectToAction("Create");
+        }
+
+
+        public async Task<IActionResult> ConfirmOrder()
+        {
+            var user = await this.userHelper.GetUserByEmailAsync(this.User.Identity.Name);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var orderDetailTemp = await this.datacontext.OrderDetailTemps
+                .Include(odt => odt.Product)
+                .Where(odt => odt.User == user)
+                .ToListAsync();
+
+            if(orderDetailTemp == null || orderDetailTemp.Count == 0)
+            {
+                return NotFound();
+            }
+
+            var details = orderDetailTemp.Select(odt => new OrderDetail
+            {
+                UnitPrice = odt.UnitPrice,
+                Product = odt.Product,
+                Quantity = odt.Quantity
+
+
+            }).ToList();
+
+            var order = new Order
+            {
+                OrderDate = DateTime.UtcNow,
+                User = user,
+                DeliveryDate = DateTime.UtcNow,
+                Items = details
+            };
+
+            this.datacontext.Orders.Add(order);
+            this.datacontext.OrderDetailTemps.RemoveRange(orderDetailTemp);
+            await this.datacontext.SaveChangesAsync();
+            return this.RedirectToAction("Index");
+            
+
 
 
         }
